4f5eaf510c1ca6fd6efb97b0b273e7ab72c3042b
edwardAugust <edward.aug.watts@gmail.com>
2023-04-20 19:06:56 -0500

    update panel table

--- infographic.js
+++ infographic.js
@@ -1,7 +1,16 @@
 function buildPanel(data){
-    console.log(data)
-    // div.innerHTML 
-} 
+    console.log(data['Top Wines'][0]);
+    let table = d3.select('table');
+    table.attr("class", "table table-striped");
+    console.log(data);
+    // Use D3 to select the table body
+    let tbody = d3.select("tbody");
+    //loop over data
+    for(i = 0; i < data['Top Wines'].length; i++){
+        let row = tbody.append("tr");
+        row.append("td").text(data['Top Wines'][i]['wine']);
+    };
+}
 
 function makeInfographic(){
     let list = document.getElementById("pairing-dropdown");
