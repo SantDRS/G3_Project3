a302eb54b025d41eab8e1a9d749af1673531806b
Ricky Garcia <rickygarcia@Rickys-Air.attlocal.net>
2023-04-23 14:42:39 -0500

    Updated HTML and BAR

--- js/P3_G3_bar.js
+++ js/P3_G3_bar.js
@@ -5,7 +5,7 @@ let books = ["France", "United States", "Germany", "Italy", "Austria", "Argentin
 
 let timesRead = [627,321, 42, 31, 16, 10, 40];
 
-let colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2"];
+let colors = ["#722F37", "#722F37", "#722F37", "#722F37", "#722F37", "#722F37", "#722F37"];
 
 let trace1 = {
   x: books,
