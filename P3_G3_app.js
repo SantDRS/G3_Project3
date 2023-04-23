cf7f0f365d90dc49c0b35a01a118c9beccf12a59
Rojodu <robjdupont@gmail.com>
2023-04-23 13:15:41 -0500

    Cleaning up repository

--- P3_G3_app.js
+++
@@ -1,6 +0,0 @@
-const BaseUrl = 'http://127.0.0.1:5000/pairing_collection';
-
-// Fetch url data & log it
-d3.json(BaseUrl).then(function(data){
-  console.log(data);
-});
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # G3_Project3
 Group 3 - Project3
+
+We did some things we are not proud of :(
